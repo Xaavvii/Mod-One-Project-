7bf5180b4f6e4a68aad3adf24eb99ba10abcedd8
github username <github email address>
2019-04-16 16:49:32 -0400

    created Pizza Toppings Associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,55 @@
 require_relative "../lib/models/pizza.rb"
 require_relative "../lib/models/topping.rb"
 
-beef = Topping.find_or_create_by(name: "Beef")
-extra_cheese = Topping.find_or_create_by(name: "Extra Cheese")
-sausage = Topping.find_or_create_by(name: "Sausage")
-bacon = Topping.find_or_create_by(name: "Bacon")
-onions = Topping.find_or_create_by(name: "Onions")
-mushrooms = Topping.find_or_create_by(name: "Mushrooms")
-pepperoni = Topping.find_or_create_by(name: "Pepperoni")
-black_olives = Topping.find_or_create_by(name: "Black Olives")
-green_peppers = Topping.find_or_create_by(name: "Green Peppers")
-red_peppers = Topping.find_or_create_by(name: "Red Peppers")
-pineapple = Topping.find_or_create_by(name: "Pineapple")
-spinach = Topping.find_or_create_by(name: "Spinach")
-ham = Topping.find_or_create_by(name: "Ham")
-philly_steak = Topping.find_or_create_by(name: "Philly Steak")
+#Toppings
+beef = Topping.find_or_create_by(name: "Beef") #1
+Topping.find_or_create_by(name: "Extra Cheese") #2
+Topping.find_or_create_by(name: "Sausage") #3
+Topping.find_or_create_by(name: "Bacon") #4
+Topping.find_or_create_by(name: "Onions") #5
+Topping.find_or_create_by(name: "Mushrooms") #6
+Topping.find_or_create_by(name: "Pepperoni") #7
+Topping.find_or_create_by(name: "Black Olives") #8
+Topping.find_or_create_by(name: "Green Peppers") #9
+Topping.find_or_create_by(name: "Red Peppers") #10
+Topping.find_or_create_by(name: "Pineapple") #11
+Topping.find_or_create_by(name: "Spinach") #12
+Topping.find_or_create_by(name: "Ham") #13
+Topping.find_or_create_by(name: "Philly Steak") #14
 
-meatzza = Pizza.find_or_create_by(name: "MeatZZa")
-extravanganZZa = Pizza.find_or_create_by(name: "ExtravaganZZa")
-philly_cheese = Pizza.find_or_create_by(name: "Philly Cheese")
-honolulu= Pizza.find_or_create_by(name: "Honolulu")
+#Specialty Pizzas
+Pizza.find_or_create_by(name: "MeatZZa") #1
+Pizza.find_or_create_by(name: "ExtravaganZZa") #2
+Pizza.find_or_create_by(name: "Philly Cheese") #3
+Pizza.find_or_create_by(name: "Honolulu") #4
+
+
+#####PIZZATOPPINGS ASSOCIATIONS#######
+
+#Meatzza
+PizzaTopping.find_or_create_by(pizza_id: 1, topping_id: 1)
+PizzaTopping.find_or_create_by(pizza_id: 1, topping_id: 3)
+PizzaTopping.find_or_create_by(pizza_id: 1, topping_id: 7)
+PizzaTopping.find_or_create_by(pizza_id: 1, topping_id: 13)
+
+#ExtravaganZZa
+PizzaTopping.find_or_create_by(pizza_id: 2, topping_id: 5)
+PizzaTopping.find_or_create_by(pizza_id: 2, topping_id: 9)
+PizzaTopping.find_or_create_by(pizza_id: 2, topping_id: 8)
+PizzaTopping.find_or_create_by(pizza_id: 2, topping_id: 6)
+PizzaTopping.find_or_create_by(pizza_id: 2, topping_id: 1)
+PizzaTopping.find_or_create_by(pizza_id: 2, topping_id: 13)
+PizzaTopping.find_or_create_by(pizza_id: 2, topping_id: 7)
+PizzaTopping.find_or_create_by(pizza_id: 2, topping_id: 3)
+
+#PhillyCheese
+PizzaTopping.find_or_create_by(pizza_id: 3, topping_id: 14)
+PizzaTopping.find_or_create_by(pizza_id: 3, topping_id: 5)
+PizzaTopping.find_or_create_by(pizza_id: 3, topping_id: 6)
+PizzaTopping.find_or_create_by(pizza_id: 3, topping_id: 9)
+
+#Honolulu
+PizzaTopping.find_or_create_by(pizza_id: 4, topping_id: 11)
+PizzaTopping.find_or_create_by(pizza_id: 4, topping_id: 13)
+PizzaTopping.find_or_create_by(pizza_id: 4, topping_id: 4)
+PizzaTopping.find_or_create_by(pizza_id: 4, topping_id: 10)
